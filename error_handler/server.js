5b85216805eef92d44b3a052d3d87f612056fdd3
Mozdalif sikder <mozsocia@gmail.com>
2024-08-02 15:49:59 +0600

    done

--- error_handler/server.js
+++ error_handler/server.js
@@ -1,13 +1,22 @@
 const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose');
+
+// Connect to MongoDB mongoose.connect('mongodb://127.0.0.1:27017/test1')
+mongoose.connect('mongodb://127.0.0.1:27017/error_test')
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+// Define a schema
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }
+});
+
+const User = mongoose.model('User', UserSchema);
 
 const errorHandler = (error, request, reply) => {
-  // Log the error (you might want to use a proper logging library in production)
-  console.error(error);
-
-  // Determine the status code
+  // console.error(error);
   const statusCode = error.statusCode || 500;
-
-  // Send the error response
   reply.status(statusCode).send({
     error: {
       message: error.message || 'Internal Server Error',
@@ -26,7 +35,6 @@ fastify.register(async (instance, opts) => {
   });
 });
 
-
 class CustomError extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -36,6 +44,16 @@ class CustomError extends Error {
 
 fastify.get('/custom-error', async (request, reply) => {
   throw new CustomError('Custom error message', 400);
+});
+
+fastify.post('/create-user', async (request, reply) => {
+  const userData = request.body;
+  
+  
+    const user = new User(userData);
+    await user.save();
+    reply.code(201).send(user);
+
 });
 
 // Start server
